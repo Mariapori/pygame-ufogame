6a05d306ae8fb136cc4ec2fcd1e76b5e4ef20c80
Topias Mariapori <topias@mariapori.fi>
2021-06-12 22:08:10 +0300

    Discord integraatio

--- peli.py
+++ peli.py
@@ -3,6 +3,8 @@ import os
 import sys
 import random
 import threading
+import discordsdk as sdk
+import time
 
 pygame.init()
 pygame.font.init()
@@ -16,7 +18,8 @@ spawnevent = pygame.USEREVENT + 1
 clock = pygame.time.Clock()
 pygame.display.set_caption("Testipeli")
 miinat = []
-
+app = sdk.Discord(0, sdk.CreateFlags.default)
+activity_manager = app.get_activity_manager()
 osuma = pygame.mixer.Sound("hit.wav")
 
 
@@ -33,6 +36,7 @@ class Miina:
         self.x = x
         self.y = y
 def main():
+    activity = sdk.Activity()
     run = True
     pelaaja1 = Pelaaja(1,1,100)
     font = pygame.font.SysFont("comicsans",30)
@@ -67,7 +71,8 @@ def main():
                 pygame.time.set_timer(spawnevent, 500)
 
         txtHealth = font.render("HP: " + str(pelaaja1.health),True, (255,255,255))
-            
+        activity.state = "HP: " + str(pelaaja1.health)
+        activity_manager.update_activity(activity, callback)
         pelaajarect = pygame.Rect(pelaaja1.x,pelaaja1.y, ufo.get_width(),ufo.get_height())
         handle(pelaajarect)
         draw(txtHealth)
@@ -86,6 +91,11 @@ def main():
         if keys[pygame.K_DOWN] and pelaaja1.y + ufo.get_height() < HEIGHT:
             pelaaja1.y = pelaaja1.y + 3
         pygame.display.update()
-
+        app.run_callbacks()
+def callback(result):
+    if result == sdk.Result.ok:
+        print("Successfully set the activity!")
+    else:
+        raise Exception(result)
 
 main()
